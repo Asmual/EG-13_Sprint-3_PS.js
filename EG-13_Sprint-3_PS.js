42b275dfaa1d6a6f302d3f591580cf620ce93735
Asmual Obaidul Hoque <73558650+Asmual@users.noreply.github.com>
2026-09-04 21:40:30 +0600

    Add solution for Move Zeroes problem

--- EG-13_Sprint-3_PS.js
+++ EG-13_Sprint-3_PS.js
@@ -12,3 +12,21 @@ var containsDuplicate = function(nums) {
 // Test Case: 
 // console.log(containsDuplicate([1, 2, 3, 1])); 
 // Expected Output: true
+
+// 02. Move Zeroes
+var moveZeroes = function(nums) {
+    let insertPos = 0;
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] !== 0) {
+            nums[insertPos] = nums[i];
+            insertPos++;
+        }
+    }
+    for (let i = insertPos; i < nums.length; i++) {
+        nums[i] = 0;
+    }
+};
+// Test Case: 
+// let arr = [0, 1, 0, 3, 12]; moveZeroes(arr);
+// console.log(arr); 
+// Expected Output: [1, 3, 12, 0, 0]
