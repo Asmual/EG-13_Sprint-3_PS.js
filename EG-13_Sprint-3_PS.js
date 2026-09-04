31cc24417abc195cbafc3926f24dcf9b0c263e54
Asmual Obaidul Hoque <73558650+Asmual@users.noreply.github.com>
2026-09-04 21:37:25 +0600

    Add solution for Contains Duplicate problem

--- EG-13_Sprint-3_PS.js
+++ EG-13_Sprint-3_PS.js
@@ -1,1 +1,14 @@
-console.log("EG-13_Sprint-3_PS");
+// 01. Contains Duplicate
+var containsDuplicate = function(nums) {
+    let seen = {};
+    for (let i = 0; i < nums.length; i++) {
+        if (seen[nums[i]]) {
+            return true;
+        }
+        seen[nums[i]] = true;
+    }
+    return false;
+};
+// Test Case: 
+// console.log(containsDuplicate([1, 2, 3, 1])); 
+// Expected Output: true
