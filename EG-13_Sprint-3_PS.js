9a5c7eb5f1aa0b90aa4afa038f2ddacba0b08c36
Asmual Obaidul Hoque <73558650+Asmual@users.noreply.github.com>
2026-09-04 21:41:05 +0600

    Add solution for Valid Anagram problem

--- EG-13_Sprint-3_PS.js
+++ EG-13_Sprint-3_PS.js
@@ -30,3 +30,22 @@ var moveZeroes = function(nums) {
 // let arr = [0, 1, 0, 3, 12]; moveZeroes(arr);
 // console.log(arr); 
 // Expected Output: [1, 3, 12, 0, 0]
+
+
+// 03. Valid Anagram
+var isAnagram = function(s, t) {
+    if (s.length !== t.length) return false;
+    let count = {};
+    for (let char of s) {
+        count[char] = (count[char] || 0) + 1;
+    }
+    for (let char of t) {
+        if (!count[char]) return false;
+        count[char]--;
+    }
+    return true;
+};
+// Test Case: 
+// console.log(isAnagram("anagram", "nagaram")); 
+// Expected Output: true
+
